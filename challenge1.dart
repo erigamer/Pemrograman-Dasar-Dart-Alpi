0e1546df110b88930e67a49e230dfac506b912b5
erigamer <150240725+erigamer@users.noreply.github.com>
2024-03-12 18:36:36 +0700

    Alfikri Maulana-Dart commit

---
+++ challenge1.dart
@@ -0,0 +1,34 @@
+void main() {
+
+
+  String nama = 'al_';
+  int tahun = 2000;
+  String pemilik = 'al_';
+  String alamat = 'Jl. Bhayangkara, Jakarta';
+  String telepon = '08123456789';
+  bool statusBuka = true;
+  List<Map> daftarMakanan = [
+    {'nama': 'Kepiting Rebus', 'harga': 40000},
+    {'nama': 'Nasi Goreng', 'harga': 20000},
+    {'nama': 'Udang Asam Manis', 'harga': 50000},
+    {'nama': 'Sate Cumi', 'harga': 30000}
+  ];
+  List<Map> daftarMinuman = [
+    {'nama': 'Es Jeruk', 'harga': 5000},
+    {'nama': 'Es Kelapa', 'harga': 10000},
+    {'nama': 'Es Teh', 'harga': 3000},
+  ];
+
+  Map restoran = {
+    'nama': nama,
+    'tahun': tahun,
+    'pemilik': pemilik,
+    'alamat': alamat,
+    'telepon': telepon,
+    'statusBuka': statusBuka,
+    'daftarMakanan': daftarMakanan,
+    'daftarMinuman': daftarMinuman
+  };
+
+  print(restoran);
+}
---
+++ for_looping.dart
@@ -0,0 +1,26 @@
+void main() {
+ 
+
+  // Manual
+  print('Manual');
+  print(1);
+  print(2);
+  print(3);
+
+  // Menggunakan For
+  print('Menggunakan For');
+  int n = 100;
+  for (int i = 1; i <= n; i++) {
+    if (i % 2 == 1) {
+      print(i);
+    }
+  }
+
+  // Menggunakan for pada list
+  List daftarMakanan = ['Sate', 'Nasi Goreng', 'Bakso'];
+  print('Daftar Makanan');
+  print(daftarMakanan);
+  for (int i = 0; i < daftarMakanan.length; i++) {
+    print(daftarMakanan[i]);
+  }
+}
